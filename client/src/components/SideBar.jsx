3ed1037f1fc0d55ed8c8823019ea447f6cd6c495
Aman Farkya <amanfarkya143@gmail.com>
2026-07-13 15:17:38 +0530

    setup for the SideBar is done, remove the dummy data (compare with the previous commit to see the actual difference in the code)

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,12 +1,33 @@
-import { useContext } from "react";
-import assets, { userDummyData } from "../assets/assets.js";
+import { useContext, useEffect, useState } from "react";
+import assets from "../assets/assets.js";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext.jsx";
+import { ChatContext } from "../../context/ChatContext.jsx";
 
-const SideBar = ({ selectedUser, setSelectedUser }) => {
+const SideBar = () => {
   const navigate = useNavigate();
 
-  const { logout } = useContext(AuthContext);
+  const { logout, onlineUsers } = useContext(AuthContext);
+  const {
+    getUsers,
+    users,
+    selectedUser,
+    setSelectedUser,
+    unseenMessages,
+    setUnseenMessages,
+  } = useContext(ChatContext);
+
+  const [input, setInput] = useState("");
+
+  const filteredUser = input
+    ? users.filter((user) =>
+        user.fullName.toLowerCase().includes(input.toLowerCase()),
+      )
+    : users;
+
+  useEffect(() => {
+    getUsers();
+  }, [onlineUsers]);
 
   return (
     <div
@@ -42,12 +63,14 @@ const SideBar = ({ selectedUser, setSelectedUser }) => {
             type="text"
             className="bg-transparent border-none outline-none text-white text-xs placeholder-[#c8c8c8] flex-1"
             placeholder="Search User..."
+            onChange={(e) => setInput(e.target.value)}
+            // value={input}
           />
         </div>
       </div>
 
       <div className="flex flex-col">
-        {userDummyData.map((user, idx) => (
+        {filteredUser.map((user, idx) => (
           <div
             key={idx}
             onClick={() => {
@@ -64,17 +87,17 @@ const SideBar = ({ selectedUser, setSelectedUser }) => {
             />
             <div className="flex flex-col leading-5">
               <p>{user.fullName}</p>
-              {idx < 3 ? (
+              {onlineUsers.includes(user._id) ? (
                 <span className="text-green-400 text-xs">Online</span>
               ) : (
                 <span className="text-neutral-400 text-xs">Offline</span>
               )}
             </div>
 
-            {idx > 2 && (
+            {unseenMessages[user._id] > 0 && (
               <p className="absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-violet-500/50">
                 {" "}
-                {idx}
+                {unseenMessages[user._id]}
               </p>
             )}
           </div>
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -12,8 +12,8 @@ function HomePage() {
         className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-full grid grid-cols-1 relative ${selectedUser ? "md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]" : "md:grid-cols-2"} `}
       >
         <SideBar
-          selectedUser={selectedUser}
-          setSelectedUser={setSelectedUser}
+        // selectedUser={selectedUser}
+        // setSelectedUser={setSelectedUser}
         />
         <ChatContainer
           selectedUser={selectedUser}
